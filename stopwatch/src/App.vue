80ee88a9ecb3d74ce8d82053acfb1cba540a9b34
Dmitriy Adieliev <d.adieliev@urancompany.com>
2024-07-27 11:46:14 +0300

    init stopwatch

---
+++ stopwatch/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="application-wrapper">
+    <header></header>
+    <main>dwqdwqdqw</main>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
+
+<style scoped></style>
